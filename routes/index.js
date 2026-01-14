b2561bbd5e69dd4fc5c830c471b1017b7be252be
Michele Argenti <michele.argenti000@gmail.com>
2026-01-14 16:24:34 +0100

    Delete routes directory

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const routes = require('express').Router();
-const lesson1Controller = require('../controllers/lesson1');
-
-routes.get('/', (req, res) => {
-  res.send("Hello");
-});
-routes.get('/test', lesson1Controller.testRoute);
-
-module.exports = routes;
